6cd72baebc3ba283ee12083c9e41814ae7cdfe01
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-26 17:11:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,37 @@
-
+  def my_languages
+  {
+      :oo => {
+        :ruby => {
+          :type => "interpreted"
+        },
+        :javascript => {
+          :type => "interpreted"
+        },
+        :python => {
+          :type => "interpreted"
+        },
+        :java => {
+          :type => "compiled"
+        }
+      },
+      :functional => {
+        :clojure => {
+          :type => "compiled"
+        },
+        :erlang => {
+          :type => "compiled"
+        },
+        :scala => {
+          :type => "compiled"
+        },
+        :javascript => {
+          :type => "interpreted"
+        }
+     
+      }
+    }
+  }
+end
 
 def reformat_languages(languages)
   new_hash = {}
