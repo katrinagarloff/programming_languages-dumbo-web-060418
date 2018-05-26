0050ff633b672f914a82b3ca37e30244181b5424
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-26 17:45:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,46 +1,9 @@
-  def my_languages
-  {
-      :oo => {
-        :ruby => {
-          :type => "interpreted"
-        },
-        :javascript => {
-          :type => "interpreted"
-        },
-        :python => {
-          :type => "interpreted"
-        },
-        :java => {
-          :type => "compiled"
-        }
-      },
-      :functional => {
-        :clojure => {
-          :type => "compiled"
-        },
-        :erlang => {
-          :type => "compiled"
-        },
-        :scala => {
-          :type => "compiled"
-        },
-        :javascript => {
-          :type => "interpreted"
-      }
-    }
-  }
-end
-
-def reformat_languages(languages)
+ def reformat_languages(languages)
   new_hash = {}
-  extra_style = []
+  
   languages.each do |style_key, language|
     language.each do |language_key, data_type|
       data_type.each do |data_type_key, data|
-        #puts language_key
-        #puts style_key
-        #puts data_type_key
-        #puts data
         if new_hash[language_key]
           new_hash[language_key][:style] << style_key
         else
@@ -52,4 +15,3 @@ def reformat_languages(languages)
   new_hash
 end
 
-reformat_languages(my_languages)
