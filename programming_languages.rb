ced2371cab2dfc83984593078269478f0b8e3f50
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-26 17:16:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,7 +36,8 @@ def reformat_languages(languages)
   
   languages.each do |style_key, language|
     language.each do |language_key, data_type|
-    puts data_type
+      data_type.each do |data_type_key, data|
+    puts data_type_key
   end
   end
 end
